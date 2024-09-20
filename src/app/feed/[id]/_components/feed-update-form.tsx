36b376dc02d2fa057729b86258133dbd9f28bf8f
rayyo999 <eraytsai@gmail.com>
2024-09-20 22:16:35 +0100

    fix: resolve NextRouter mounting issue

--- src/app/feed/[id]/_components/feed-update-form.tsx
+++ src/app/feed/[id]/_components/feed-update-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import {
   AlertDialog,
