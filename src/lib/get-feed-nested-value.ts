d9051cee8d6175d2f7ef821c3aa5965ebe07df28
rayyo999 <eraytsai@gmail.com>
2024-10-11 14:01:09 +0100

    fix: handle array in feed nested value

--- src/lib/get-feed-nested-value.ts
+++ src/lib/get-feed-nested-value.ts
@@ -3,7 +3,18 @@ import type { RemoteFeed } from "~/types";
 export function getFeedNestedValue<T>(feed: T, keyPath: string): unknown {
   return keyPath.split(".").reduce<unknown>((acc, part) => {
     if (acc && typeof acc === "object" && part in acc) {
-      return (acc as RemoteFeed)[part];
+      const value = (acc as RemoteFeed)[part];
+
+      if (Array.isArray(value)) {
+        return value
+          .flat()
+          .map((item) =>
+            typeof item === "string" ? item.trim() : JSON.stringify(item),
+          )
+          .join(", ");
+      }
+
+      return typeof value === "string" ? value.trim() : value;
     }
     return undefined;
   }, feed);
